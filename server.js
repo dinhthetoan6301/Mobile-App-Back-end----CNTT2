9922f921cf098b601da964fb47d88e820b12801f
thetoan2001 <augus19995@gmail.com>
2024-08-07 02:40:07 +0700

    update server.js

--- server.js
+++ server.js
@@ -13,12 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
     res.send('Job Finder API is running');
