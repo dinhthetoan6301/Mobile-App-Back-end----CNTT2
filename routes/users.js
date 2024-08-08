f702311f445205b981ab775f83e3e584e710edbb
thetoan2001 <augus19995@gmail.com>
2024-08-09 03:16:13 +0700

    Fix userController import and update related files

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-// Import các controller function
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/signup', registerUser);
-router.post('/signin', loginUser);
+router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
